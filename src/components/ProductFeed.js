020e3093ecf58b5b2c1d17dbeac5115cb79b4581
nayana.piumal <nayana.piumal@hitachi-dps.com>
2022-10-20 23:20:20 +0530

    product component and product page implement

--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Product from './Product'
 
-function ProductFeed() {
+function ProductFeed({ products }) {
+    console.log("items product", products)
+
   return (
-    <div>
-      product feed
+    <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto'>
+      {products.slice(0,4).map(product => (
+        <Product key={product?.id} product={product} />
+      ))}
+
+      <img className='md:col-span-full' src="https://links.papareact.com/dyz" alt="" />
+
+      <div className='md:col-span-2'>
+        {products.slice(4,5).map(product => (
+            <Product key={product?.id} product={product} />
+        ))}
+      </div>
+
+      {products.slice(5,products.lenght).map(product => (
+            <Product key={product?.id} product={product} />
+        ))}
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,8 @@ import Header from "../components/Header";
 import Banner from "../components/Banner";
 import ProductFeed from "../components/ProductFeed";
 
-export default function Home() {
+export default function Home({products}) {
+  
   return (
     <div className="bg-gray-100">
       <Head>
@@ -17,8 +18,20 @@ export default function Home() {
         {/* banner */}
         <Banner />
         {/* product feed */}
-        <ProductFeed />
+        <ProductFeed products={products} />
       </main>
     </div>
   );
+}
+
+
+export async function getServerSideProps(context){
+  const products = await fetch('https://fakestoreapi.com/products')
+  .then(res=>res.json());
+
+  return {
+    props :{
+      products:products
+    }
+  }
 }
